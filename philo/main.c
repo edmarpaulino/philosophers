aaf37c3cb9ef1ed7ffb36bdf77b32bf1bb5e0545
Edmar Paulino <edmarpaulino9@gmail.com>
2022-03-27 21:10:41 -0300

    add test script

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 14:41:53 by edpaulin          #+#    #+#             */
-/*   Updated: 2022/03/27 20:15:01 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/03/27 20:54:32 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,12 @@ int	main(int argc, char *argv[])
 		printf("error: invalid input\n");
 		return (2);
 	}
-	if (argc == 5)
-	{
-		printf("number of philosophers: %s\n", argv[1]);
-		printf("time to die: %s\n", argv[2]);
-		printf("time to eat: %s\n", argv[3]);
-		printf("time to sleep: %s\n", argv[4]);
-	}
-	if (argc == 6)
-	{
-		printf("number of philosophers: %s\n", argv[1]);
-		printf("time to die: %s\n", argv[2]);
-		printf("time to eat: %s\n", argv[3]);
-		printf("time to sleep: %s\n", argv[4]);
-		printf("number of times each philosopher must eat: %s\n", argv[5]);
-	}
 	if (are_inputs_valid(&argv[1]))
-		printf(GREEN "All inputs are valid!\n\n" RESET);
+		printf(GREEN "All inputs are valid\n" RESET);
 	else
-		printf(RED "Error!\n\n" RESET);
+	{
+		printf(RED "Error: Invalid arguments\n" RESET);
+		return (1);
+	}
 	return (0);
 }
