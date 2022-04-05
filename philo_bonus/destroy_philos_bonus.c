a2762b4a24bb9663f6cefb84d37a757e36fbb0c6
Edmar Paulino <edmarpaulino9@gmail.com>
2022-04-05 10:47:46 -0300

    add checks null in destroy philos

--- philo_bonus/destroy_philos_bonus.c
+++ philo_bonus/destroy_philos_bonus.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 19:16:15 by edpaulin          #+#    #+#             */
-/*   Updated: 2022/04/04 20:12:26 by edpaulin         ###   ########.fr       */
+/*   Updated: 2022/04/05 10:47:16 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	destroy_philos(t_philo *philos)
 {
 	t_data	*data;
 
+	if (!philos)
+		return ;
 	data = philos->data;
 	free(philos);
 	destroy_data(data);
